69242bba7670edc45f0998963db3f5a9b12bd266
Yunhao Jiang <account@yunhao.email>
2023-02-07 11:05:56 -0600

    Complete Q1

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import string
 
 
 def get_parameter_vectors():
@@ -10,41 +11,50 @@ def get_parameter_vectors():
 
     Returns: tuple of vectors e and s
     '''
-    #Implementing vectors e,s as lists (arrays) of length 26
-    #with p[0] being the probability of 'A' and so on
-    e=[0]*26
-    s=[0]*26
+    # Implementing vectors e,s as lists (arrays) of length 26
+    # with p[0] being the probability of 'A' and so on
+    e = [0] * 26
+    s = [0] * 26
 
-    with open('e.txt',encoding='utf-8') as f:
+    with open('e.txt', encoding='utf-8') as f:
         for line in f:
-            #strip: removes the newline character
-            #split: split the string on space character
-            char,prob=line.strip().split(" ")
-            #ord('E') gives the ASCII (integer) value of character 'E'
-            #we then subtract it from 'A' to give array index
-            #This way 'A' gets index 0 and 'Z' gets index 25.
-            e[ord(char)-ord('A')]=float(prob)
+            # strip: removes the newline character
+            # split: split the string on space character
+            char, prob = line.strip().split(" ")
+            # ord('E') gives the ASCII (integer) value of character 'E'
+            # we then subtract it from 'A' to give array index
+            # This way 'A' gets index 0 and 'Z' gets index 25.
+            e[ord(char) - ord('A')] = float(prob)
     f.close()
 
-    with open('s.txt',encoding='utf-8') as f:
+    with open('s.txt', encoding='utf-8') as f:
         for line in f:
-            char,prob=line.strip().split(" ")
-            s[ord(char)-ord('A')]=float(prob)
+            char, prob = line.strip().split(" ")
+            s[ord(char) - ord('A')] = float(prob)
     f.close()
 
-    return (e,s)
+    return (e, s)
+
 
 def shred(filename):
-    #Using a dictionary here. You may change this to any data structure of
-    #your choice such as lists (X=[]) etc. for the assignment
-    X=dict()
-    with open (filename,encoding='utf-8') as f:
-        # TODO: add your code here
+    # Using a dictionary here. You may change this to any data structure of
+    # your choice such as lists (X=[]) etc. for the assignment
+    X = dict.fromkeys(string.ascii_uppercase, 0)
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            for letter in line:
+                if letter.isalpha():
+                    X[letter.upper()] += 1
 
     return X
 
 
+# Q1
+letter_dict = shred("samples/letter2.txt") # TODO: Change this to letter.txt when submit]
+print("Q1")
+for key in letter_dict.keys():
+    print(f"{key} {letter_dict[key]}")
 
-# TODO: add your code here for the assignment
-# You are free to implement it as you wish!
-# Happy Coding!
+# Q2
+
+
