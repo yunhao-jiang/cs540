43b876ffa0f991d18773965cd17402c87762b866
Yunhao Jiang <account@yunhao.email>
2023-02-21 12:15:40 -0600

    Create functions

---
+++ HW4/hw4.py
@@ -0,0 +1,18 @@
+def load_data(filepath):
+    pass
+
+
+def calc_features(row):
+    pass
+
+
+def hac(features):
+    pass
+
+
+def imshow_hac(Z, names):
+    pass
+
+
+if __name__ == '__main__':
+    pass
