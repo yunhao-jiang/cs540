d73190324ff5f75838788c16b5f75ecda541c91b
Yunhao Jiang <account@yunhao.email>
2023-02-21 14:54:04 -0600

    Complete step 1 and 2

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,9 +1,20 @@
+import csv
+import numpy as np
+
+
 def load_data(filepath):
-    pass
+    """Return a list of dictionaries, where each row in the dataset is a dictionary with
+    the column headers as keys and the row elements as values. """
+    with open(filepath) as f:
+        reader = csv.DictReader(f)
+        data = [row for row in reader]
+    return data
 
 
 def calc_features(row):
-    pass
+    """Return Attack, Sp. Atk, Speed, Defense, Sp. Def, and HP as a numpy array."""
+    return np.array([row['Attack'], row['Sp. Atk'], row['Speed'], row['Defense'], row['Sp. Def'],
+                     row['HP']]).astype(int)
 
 
 def hac(features):
@@ -15,4 +26,5 @@ def imshow_hac(Z, names):
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('Pokemon.csv')
+    print(calc_features(data[0]))
